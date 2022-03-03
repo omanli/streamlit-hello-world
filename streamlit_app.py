16f520ff5b01b201f1cada4ad8f643cd4c3e28ed
Murat Anlı <52565584+omanli@users.noreply.github.com>
2022-03-03 17:36:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ with dfo:
 A = st.slider("Awesomeness", value=80, min_value=0, max_value=120, step=5, format="%d", on_change=cb)
 if A > 90:
   st.write("Awesome!")
+ else:
+  st.write("meh..")
 
 SD = st.checkbox("SuperDuper", value=False, on_change=None)
 if SD:
