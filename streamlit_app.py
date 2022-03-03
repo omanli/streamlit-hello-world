f6a1b61f4ec54c225d0b56046a517fc97d583057
Murat Anlı <52565584+omanli@users.noreply.github.com>
2022-03-03 17:25:19 +0300

    Update streamlit_app.py
    update output of dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 def cb():
-  global df
+  global df, dfo
   df.Col3[4] += 1
-
+  dfo.text_area(df)
 
 st.write("""
 # Hello, world!
@@ -13,8 +13,8 @@ Here is my *first* app
 
 st.write("Here is a Dataframe")
 df = pd.DataFrame({"Col1": [1, 2, 3, 4, 5], "Col2": [4, 5, 6, 7, 8], "Col3": [7, 8, 9, 0, 1]})
-st.write(df)
-
+dfo = st.empty()
+dfo.text_area(df)
 
 A = st.slider("Awesomeness", value=80, min_value=0, max_value=120, step=5, format="%d", on_change=cb)
 if A > 90:
