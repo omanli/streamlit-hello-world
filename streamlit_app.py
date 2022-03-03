dfe2e426e50e21aad5bdbc4c22894ab7a3435a47
Murat Anlı <52565584+omanli@users.noreply.github.com>
2022-03-03 15:41:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,12 @@ st.write("""
 Here is my *first* app
 """)
 
-A = st.slider("Awesomeness", min_value=0, max_value=120, step=5, on_change=None)
+A = st.slider("Awesomeness", value=80, min_value=0, max_value=120, step=5, on_change=None)
+if A > 90:
+  st.write("Awesome!")
 
 SD = st.checkbox("SuperDuper", value=False, on_change=None)
+if SD:
+  st.write("Yes!")
+else:
+  st.write("Are you sure this is not Super Duper?")
